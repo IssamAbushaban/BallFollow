9a6b49ce39c6d991b3670bac17b8a6dcfbb6f3f4
Issam <iba10@pitt.edu>
2021-03-09 22:26:01 -0500

    Options!
    Added more variability.
    Fun different outcomes!

--- BallFollow/Ball.swift
+++ BallFollow/Ball.swift
@@ -5,15 +5,18 @@
 //  Created by Issam Abushaban on 3/2/21.
 //
 
-import CoreGraphics
+import AppKit
 
 /// The `Ball` class with a position, velocity and size.
 public struct Ball {
-    public var position: CGPoint = .zero
-    public var velocity: CGVector = .zero
-
+    public  var position: CGPoint  = .zero
+    public  var velocity: CGVector = .zero
+    public  var color:    NSColor  = .white
+    public  var radius:   CGFloat  = 15
     //Default Constructor
-    init()  {
+    init(_ color: NSColor,radius: CGFloat)  {
+        self.color = color
+        self.radius = abs(radius + 1)
     }
     
     //Specific Constructor
--- BallFollow/BallFollow.swift
+++ BallFollow/BallFollow.swift
@@ -8,25 +8,29 @@ class BallFollow: ScreenSaverView {
 
     // MARK: - Variables -----------------------------------------------------------
     private var ballList:    [Ball]  = []
-    private var debugOutput: String  = ""
-    private let ballRadius:  CGFloat = 15
+    private var debugOutput:    String  = ""
+    private var colorToggle:    Bool    = true
+    private var waitCount:      Int     = 3
+    private let maxBalls:       Int     = 800
+    private let variability:    CGFloat = 20
+    private let startingSpeed:  CGFloat = 40
+    private let leadBallRadius: CGFloat = 25
     
     // MARK: - Initialization ------------------------------------------------------
     override init?(frame: NSRect, isPreview: Bool) {
         super.init(frame: frame, isPreview: isPreview)
         
         //Add the first ball to the list
-        ballList.append(Ball())
+        ballList.append(Ball(.white, radius: leadBallRadius))
         
-        //Set up the first ball
         //Set Position
-        let xInit = CGFloat.random(in: 0...frame.width)
-        let yInit = CGFloat.random(in: 0...frame.height)
+        let xInit = CGFloat.random(in: 10...(frame.width  - 10))
+        let yInit = CGFloat.random(in: 10...(frame.height - 10))
         ballList[0].position = CGPoint(x: xInit,y: yInit)
         
         //Set Velocity
-        let xVelocity: CGFloat = 35
-        let yVelocity: CGFloat = 35
+        let xVelocity: CGFloat = startingSpeed
+        let yVelocity: CGFloat = startingSpeed
         let xSign: CGFloat = Bool.random() ? 1 : -1
         let ySign: CGFloat = Bool.random() ? 1 : -1
         ballList[0].velocity = CGVector(dx: xVelocity * xSign, dy: yVelocity * ySign)
@@ -47,26 +51,49 @@ class BallFollow: ScreenSaverView {
         let oobAxes = checkOutOfBounds(b: ballList[0])
         
         //With each collision add a ball
-        if oobAxes.xAxis || oobAxes.yAxis {
-            ballList.append(Ball())
+        if (oobAxes.xAxis || oobAxes.yAxis) && ballList.count < maxBalls {
+            
+            if colorToggle {
+                ballList.append(Ball(.blue, radius: CGFloat.random(in:  0...leadBallRadius)))
+                colorToggle = false
+            }
+            else {
+                ballList.append(Ball(.systemBlue, radius: CGFloat.random(in:  0...leadBallRadius)))
+                colorToggle = true
+            }
         }
 
         //For each ball
         for index in stride(from: ballList.count - 1, to: 0, by: -1) {
-            ballList[index].position = ballList[index - 1].position
+            ballList[index].position.x = ballList[index - 1].position.x + CGFloat.random(in:  -variability...variability)
+            ballList[index].position.y = ballList[index - 1].position.y + CGFloat.random(in:  -variability...variability)
             ballList[index].velocity = ballList[index - 1].velocity
+            
         }
-
+        
         //Handle the lead ball
         if oobAxes.xAxis {
             ballList[0].velocity.dx *= -1
+            
         }
+        
         if oobAxes.yAxis {
             ballList[0].velocity.dy *= -1
         }
-
-        ballList[0].position.x += ballList[0].velocity.dx
-        ballList[0].position.y += ballList[0].velocity.dy
+        
+        var x_mult: CGFloat = CGFloat.random(in:  -variability...0)
+        var y_mult: CGFloat = CGFloat.random(in:  -variability...0)
+        
+        if ballList[0].velocity.dx > 0 {
+            x_mult *= -1
+        }
+        
+        if ballList[0].velocity.dy > 0 {
+            y_mult *= -1
+        }
+        
+        ballList[0].position.x += ballList[0].velocity.dx + x_mult
+        ballList[0].position.y += ballList[0].velocity.dy + y_mult
         
         // This is for the OS to know that we wish to redraw the screen
         setNeedsDisplay(bounds)
@@ -83,10 +110,10 @@ class BallFollow: ScreenSaverView {
     // MARK: - General Helpers -----------------------------------------------------
     
     private func checkOutOfBounds(b: Ball) -> (xAxis: Bool, yAxis: Bool) {
-        let xAxisOOB = b.position.x - ballRadius <= 0 ||
-            b.position.x + ballRadius >= bounds.width
-        let yAxisOOB = b.position.y - ballRadius <= 0 ||
-            b.position.y + ballRadius >= bounds.height
+        let xAxisOOB = b.position.x - b.radius <= 0 ||
+            b.position.x + b.radius >= bounds.width
+        let yAxisOOB = b.position.y - b.radius <= 0 ||
+            b.position.y + b.radius >= bounds.height
         return (xAxisOOB, yAxisOOB)
     }
     
@@ -98,15 +125,15 @@ class BallFollow: ScreenSaverView {
 
     private func drawBall(b: Ball) {
         
-        let ballRect = NSRect(x: b.position.x - ballRadius,
-                              y: b.position.y - ballRadius,
-                              width:  ballRadius * 2,
-                              height: ballRadius * 2)
+        let ballRect = NSRect(x: b.position.x - b.radius,
+                              y: b.position.y - b.radius,
+                              width:  b.radius * 2,
+                              height: b.radius * 2)
         
         let ballSprite = NSBezierPath(roundedRect: ballRect,
-                                      xRadius: ballRadius,
-                                      yRadius: ballRadius)
-        NSColor.white.setFill()
+                                      xRadius: b.radius,
+                                      yRadius: b.radius)
+        b.color.setFill()
         ballSprite.fill()
     }
     
